f41edb48cdce21fd8b0b863697c5fa636dcf111e
edityourusername <42437515+edityourusername@users.noreply.github.com>
2021-03-16 18:12:13 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+   <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<title>AU BANK</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+   
+</head>
+<body>
+<header>
+   <div class="main">
+   	<div class="logo">
+         <img src="images/bank.png">
+   	</div>
+   	<ul>
+   			<li><a href="#">Home</a></li>
+   			<li><a href="createuser.php">Create User</a></li>
+   			<li><a href="transfermoney.php">Transfer Money</a></li>
+   			<li><a href="transactionhistory.php">Transaction History</a></li>
+   		
+   	</ul>
+   </div>
+   <div class="title">
+   	<h1>AU BANK</h1>
+   </div>
+   <div class="button">
+   	<a href="createuser.php" class="btn">Create User</a>
+   	<a href="transfermoney.php" class="btn">Transfer Money</a>
+   	<a href="transactionhistory.php" class="btn">Transaction History</a>
+   	
+   </div>
+
+</header>
+<footer>
+        <p margin-left:30px>&copy 2021 Made by <b>ABHISHEK UPMANYU</b></p>
+      </footer>
+
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+</html>
